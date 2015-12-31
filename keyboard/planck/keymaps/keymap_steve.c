4791075970a33602edb4f36ca87f9d3ca4003609
Steve Vang <vang0341@me.com>
2015-12-30 22:23:08 -0600

    Add right alt to layout

--- keyboard/planck/keymaps/keymap_steve.c
+++ keyboard/planck/keymaps/keymap_steve.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_ESC},
   {KC_BSPC, KC_A,    KC_R,    F(10),   KC_T,    KC_D,    KC_H,    KC_N,    F(11),   KC_I,    KC_O,    KC_QUOT},
   {SFT_T(KC_LPRN), KC_Z, KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_RPRN)},
-  {M(0),    KC_LCTL, KC_LALT, MO(_LW), GUI_T(KC_BSPC), KC_SPC,  KC_SPC,  LT(_RS,  KC_ENT), MO(_LW), KC_VOLD, KC_VOLU}
+  {M(0),    KC_LCTL, KC_LALT, MO(_LW), GUI_T(KC_BSPC), KC_SPC,  KC_SPC,  LT(_RS,  KC_ENT), MO(_LW), KC_VOLD, KC_VOLU, KC_RALT}
 },
 [_RS] = { /* RAISE */
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
